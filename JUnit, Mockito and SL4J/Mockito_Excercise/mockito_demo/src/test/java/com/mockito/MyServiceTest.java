a6ae92932f92d6649a01fc0b55f81bbb3e14c08a
HarshvardhanGupta-251 <harshvardhangupta751@gmail.com>
2026-07-21 00:15:23 +0530

    Mockito and Stubbing

---
+++ JUnit, Mockito and SL4J/Mockito_Excercise/mockito_demo/src/test/java/com/mockito/MyServiceTest.java
@@ -0,0 +1,22 @@
+package com.mockito;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when; 
+
+public class MyServiceTest {
+    
+
+    @Test
+    public void testExternalApi(){
+        ExternalApi mockApi= Mockito.mock(ExternalApi.class);
+
+        when(mockApi.getData()).thenReturn("Mocked Data");
+
+        MyService myService = new MyService(mockApi);
+        String result = myService.fetchData();
+        assertEquals("Mocked Data", result);
+    }
+}
