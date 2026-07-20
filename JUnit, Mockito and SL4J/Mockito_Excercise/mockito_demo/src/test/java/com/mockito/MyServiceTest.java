7d357c35d661701a79684659df1ee2c858e39f4d
HarshvardhanGupta-251 <harshvardhangupta751@gmail.com>
2026-07-21 00:56:14 +0530

    Updated Mockito Verifying Interactions

--- JUnit, Mockito and SL4J/Mockito_Excercise/mockito_demo/src/test/java/com/mockito/MyServiceTest.java
+++ JUnit, Mockito and SL4J/Mockito_Excercise/mockito_demo/src/test/java/com/mockito/MyServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when; 
 
 public class MyServiceTest {
@@ -16,7 +17,10 @@ public class MyServiceTest {
         when(mockApi.getData()).thenReturn("Mocked Data");
 
         MyService myService = new MyService(mockApi);
+
         String result = myService.fetchData();
         assertEquals("Mocked Data", result);
+
+        verify(mockApi).getData();
     }
 }
