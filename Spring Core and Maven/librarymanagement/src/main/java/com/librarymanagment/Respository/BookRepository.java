0e3974aa81bcf3c3698143e666b6308bda98d7af
HarshvardhanGupta-251 <harshvardhangupta751@gmail.com>
2026-07-22 20:54:49 +0530

    Added BookRepository

---
+++ Spring Core and Maven/librarymanagement/src/main/java/com/librarymanagment/Respository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.librarymanagment.Respository;
+
+public class BookRepository {
+
+    public BookRepository() {
+    System.out.println("BookRepository Bean Created");
+    }
+
+    public void display() {
+        System.out.println("BookRepository display method called");
+    }
+
+}
